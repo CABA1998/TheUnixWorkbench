d2f9957f8692942760df2018197553fea5ebe5ee
CABA1998 <62916471+CABA1998@users.noreply.github.com>
2020-05-17 16:47:47 -0500

    Create  guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+#The Unix Workbench
+#Information input function
+function entervalue {
+	echo "Enter a number with your guess on how many files this directory contains?"
+        read number
+}
+
+
+error=0
+while [[ $error -eq 0 ]]
+do
+	entervalue
+	if [[ $number -eq 3 ]]
+	then
+		error=1
+		echo "Congratulations, you've entered the right number."
+	elif [[ $number -lt 3 ]]
+	then
+		echo "Your number is lower"
+	else
+		echo "Your number is higher"
+	fi
+done
